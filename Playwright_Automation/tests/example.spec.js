5959844dc681b96f51b49d9be38cfcd078bf7af5
francis-gomes-esq <figomesokoye@protonmail.com>
2025-07-17 15:33:02 +0100

    chore(git): add trace files to .gitignore test: update example.spec.js logic

--- Playwright_Automation/tests/example.spec.js
+++ Playwright_Automation/tests/example.spec.js
@@ -1,19 +1,52 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+let context
+let page
+test.beforeAll(async ({browser}) => {
+	context = await browser.newContext()
+	await context.tracing.start({
+		snapshots: true,
+		screenshots: true,
+	})
+	page = await context.newPage()
+})
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.afterAll(async () => {
+	await context.tracing.stop({path: 'test2_trace.zip'})
+})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('homepage has Playwright', async ({}) => {
+	// await context.tracing.start({snapshots: true, screenshots: true})
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+	await page.goto('https://playwright.dev/')
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Playwright/)
+
+	// create locator
+	const getStarted = page.locator('text=Get Started')
+
+	// Expect an attribute "to be strictly equal" to the value
+	await expect(getStarted).toHaveAttribute('href', '/docs/intro')
+
+	// click the get started link
+	await getStarted.click()
+
+	// Expects the URL to contain intro
+	await expect(page).toHaveURL(/.*intro/)
+
+	// await context.tracing.stop({path: 'test1_trace.zip'})
+})
+
+test('get started link', async () => {
+	await page.goto('https://playwright.dev/')
+
+	// Click the get started link.
+	await page.getByRole('link', {name: 'Get started'}).click()
+
+	// Expects page to have a heading with the name of Installation.
+	await expect(
+		page.getByRole('heading', {name: 'Installation'})
+	).toBeVisible()
+})
