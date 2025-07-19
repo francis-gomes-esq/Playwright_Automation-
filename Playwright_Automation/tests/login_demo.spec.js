2398df122e54fd01f8dda5988f23cc80dc1fe838
francis-gomes-esq <figomesokoye@protonmail.com>
2025-07-19 22:02:56 +0100

    test(demo-login): add test for successful login and logout flow

--- Playwright_Automation/tests/login_demo.spec.js
+++ Playwright_Automation/tests/login_demo.spec.js
@@ -10,11 +10,28 @@ test('Demo Login Test 1', async ({page}) => {
 		.getByRole('textbox', {name: 'Enter your password'})
 		.fill('Password123')
 
-		/* 
+	/* 
 		Note: if the selector appears instantly, it may not wait! As demonstrated in the two examples below. 
 		*/
 	// await page.getByRole('link', { name: 'Sign in' }).waitFor({ state: 'visible', timeout: 5000 })
-	await page.waitForSelector('text=Sign in', { timeout: 5000 })
+	await page.waitForSelector('text=Sign in', {timeout: 5000})
 
 	await page.getByRole('link', {name: 'Sign in'}).click()
+})
+
+test.only('Demo Login Test 2', async ({page}) => {
+	page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/')
+	// await page.pause()
+
+	await page.getByRole('textbox', {name: 'Username'}).click()
+	await page.getByRole('textbox', {name: 'Username'}).fill('Admin')
+	await page.getByRole('textbox', {name: 'Password'}).click()
+	await page.getByRole('textbox', {name: 'Password'}).fill('admin123')
+	await page.getByRole('button', {name: 'Login'}).click()
+	await page
+		.getByRole('banner')
+		.getByRole('img', {name: 'profile picture'})
+		.click()
+	await page.getByText('Abanoub Admin').click()
+	await page.getByRole('menuitem', {name: 'Logout'}).click()
 })
